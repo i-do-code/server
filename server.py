d0fa7c7b9bb6557500b020a3a65ad623f839acaa
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-09-30 23:35:14 -0400

    Added cors support

--- server.py
+++ server.py
@@ -2,8 +2,11 @@ from flask import Flask
 from pymongo import MongoClient, database, collection
 import sys
 import json
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 client = MongoClient('mongodb://18.221.222.193:27017/')
 db = database.Database(client, "autocad")
@@ -12,5 +15,4 @@ collection = db.get_collection("date-pd-information")
 @app.route("/data/<pd>/<year>/<month>/<day>")
 def hello(pd, year, month, day):
     item = collection.find_one({'pd': int(pd), 'date': "{}/{}/{}".format(month, day, year)}, projection={'_id': False})
-    print(item, file=sys.stderr)
     return json.dumps(item)
